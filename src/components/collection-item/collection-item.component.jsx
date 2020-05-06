962b12bf0bdceb9444c5abc279157e5dededc95d
MeLlamoArroz <israel.huaman@ucsp.edu.pe>
2020-05-06 11:56:19 -0500

    added some styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import './collection-item.styles.scss'
+
+import CustomButton from '../custom-button/custom-button.component'
+
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ name, imageUrl, price }) => {
   return (
@@ -16,6 +19,7 @@ const CollectionItem = ({ name, imageUrl, price }) => {
         <span className='name'> { name } </span>
         <span className='price'> { price } </span>
       </div>
+      <CustomButton inverted> Add to cart </CustomButton>
     </div>
   )
 }
