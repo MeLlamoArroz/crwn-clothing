81b64ad9efdefb1af93dee6f879bc4dda5d59d4a
MeLlamoArroz <israel.huaman@ucsp.edu.pe>
2020-05-15 18:17:12 -0500

    Update shop data with data normalization

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,7 +2,13 @@ import { createSelector } from 'reselect';
 
 const selectShop = currentState => currentState.shop
 
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 )
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  )
